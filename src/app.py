c4b0465f0bcb900e17fc509907c474240e44363d
mintelekk <erikthompson12345@gmail.com>
2026-02-25 22:51:59 +0000

    Fixed double registration to only one time allowed

--- src/app.py
+++ src/app.py
@@ -59,6 +59,8 @@ def signup_for_activity(activity_name: str, email: str):
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
+    if email in activities[activity_name]["participants"]:
+        raise HTTPException(status_code=400, detail="already registered")
     # Get the specific activity
     activity = activities[activity_name]
 
